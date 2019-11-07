9028b6b9a5dfeaed4779d45cc74b05e5106d0b78
DenisGUCJ <denis.gorbatenk.main@gmail.com>
2019-11-07 16:27:39 +0100

    Lab5Ex4

--- src/PlayerOne.jsx
+++ src/PlayerOne.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class PlayerOne extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            count: 0
+        }
+    }
     ButtonText=()=>{
         var text;
         if(this.props.play)
@@ -10,6 +16,11 @@ class PlayerOne extends React.Component{
         }
         return text;
     }
+    IncrementCount=()=>{
+        this.setState({
+            count:++this.state.count
+        })
+    }
     render(){
         return (
             <div style={{ position:"relative",borderStyle: 'solid', borderWidth: 1, margin: 3 }}>
@@ -18,8 +29,12 @@ class PlayerOne extends React.Component{
                     Name: {this.props.name}
                 </div>
                 <div style={{margin:10}}>
-                    Played number of times
-                    <button style={{position: "absolute",right:"15px",bottom:"3px",margin: 10,width: 250}} onClick={this.props.Click} disabled={!this.props.play}>{this.ButtonText()}</button>
+                    Played number of times: {this.state.count}
+                    <button style={{position: "absolute",right:"15px",bottom:"3px",margin: 10,width: 250}} onClick={()=>{
+                        this.props.Click();this.IncrementCount()
+                    }} disabled={!this.props.play}>
+                        {this.ButtonText()}
+                    </button>
                 </div>
             </div>
         )        
--- src/PlayerTwo.jsx
+++ src/PlayerTwo.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class PlayerTwo extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            count: 0
+        }
+    }
     ButtonText=()=>{
         var text;
         if(!this.props.play)
@@ -9,6 +15,11 @@ class PlayerTwo extends React.Component{
             text="This player is playing now";
         return text;
     }
+    IncrementCount=()=>{
+        this.setState({
+            count:++this.state.count
+        })
+    }
     render(){
         return (
             <div style={{ position:"relative",borderStyle: 'solid', borderWidth: 1, margin: 3 }}>
@@ -16,8 +27,13 @@ class PlayerTwo extends React.Component{
                 <div style={{margin:10}}>
                     Name: {this.props.name}
                 </div>
-                <div style={{margin:10}}>Played number of times
-                    <button style={{position: "absolute",right:"15px",bottom:"3px",margin: 10,width: 250}} onClick={this.props.Click} disabled={this.props.play}>{this.ButtonText()}</button>
+                <div style={{margin:10}}>
+                    Played number of times: {this.state.count}
+                    <button style={{position: "absolute",right:"15px",bottom:"3px",margin: 10,width: 250}} onClick={()=>{
+                        this.props.Click();this.IncrementCount()
+                    }} disabled={this.props.play}>
+                        {this.ButtonText()}
+                    </button>
                 </div>
             </div>
         )
